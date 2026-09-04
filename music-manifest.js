5a64952fee3c0691aa5835b2c1d366a91817e668
DaomuP <DaomuP@users.noreply.github.com>
2026-09-04 16:01:24 +0800

    deploy: code

---
+++ music-manifest.js
@@ -0,0 +1,61 @@
+window.BGM_MANIFEST = {
+  "root": [
+    "M-Scarlet - GOTCHA (Off Vocal).mp3",
+    "M-Scarlet - Karma (Off Vocal).mp3",
+    "M-Scarlet - Pioneer Impact (Off Vocal).mp3",
+    "M-Scarlet - Ring Ring Ringの魔法 (Off Vocal).mp3",
+    "M-Scarlet - Tokyo自由系ガール (Off Vocal).mp3",
+    "M-Scarlet - カウントダウンラブ (Off Vocal).mp3",
+    "M-Scarlet - サマーサマーオーシャンパーリィバケーション (Off Vocal).mp3",
+    "M-Scarlet - 感謝のコントレイル (Off Vocal).mp3"
+  ],
+  "units": {
+    "ALSTROEMERIA": [
+      "M-Scarlet - mellow mellow (Off Vocal).mp3",
+      "M-Scarlet - パステルカラー パスカラカラー (Off Vocal).mp3",
+      "M-Scarlet - メッセージ (Off Vocal).mp3"
+    ],
+    "CoMETIK": [
+      "M-Scarlet - くだらないや (Off Vocal).mp3",
+      "M-Scarlet - アイノウ (Off Vocal).mp3",
+      "M-Scarlet - ハナムケのハナタバ (Off Vocal).mp3",
+      "M-Scarlet - 平行線の美学 (Off Vocal).mp3"
+    ],
+    "illumination STARS": [
+      "M-Scarlet - Forward March！！！ (Off Vocal).mp3",
+      "M-Scarlet - Happy Funny Lucky（Off Vocal）.mp3",
+      "M-Scarlet - KoiKyun！ (Off Vocal).mp3",
+      "M-Scarlet - We can go now!（Off Vocal）.mp3"
+    ],
+    "L'Antica": [
+      "M-Scarlet - Black Reverie（Off Vocal）.mp3",
+      "M-Scarlet - QUEEN OF THE PIRATES (Off Vocal).mp3",
+      "M-Scarlet - とある英雄たちの物語 (Off Vocal).mp3",
+      "M-Scarlet - 幻惑SILHOUETTE（Off Vocal）.mp3",
+      "M-Scarlet - 文明開花輪舞 -シティ・ハレルヤ- (Off Vocal).mp3",
+      "M-Scarlet - 銀翼のアレジアンス -blade of truth- (Off Vocal).mp3"
+    ],
+    "noctchill": [
+      "M-Scarlet - 365日のストーリー (Off Vocal).mp3",
+      "M-Scarlet - アスファルトを鳴らして(Off Vocal).mp3",
+      "M-Scarlet - 青空 (Off Vocal).mp3"
+    ],
+    "SHHis": [
+      "M-Scarlet - Fly and Fly(Off Vocal).mp3",
+      "M-Scarlet - Happier (Off Vocal).mp3",
+      "M-Scarlet - SWEETEST BITE (Off Vocal).mp3"
+    ],
+    "Straylight": [
+      "M-Scarlet - Cyber Parkour (Off Vocal).mp3",
+      "M-Scarlet - Imitation Ghost(Off Vocal).mp3",
+      "M-Scarlet - Real Mind Shakes (Off Vocal).mp3",
+      "M-Scarlet - Timeless Shooting Star (Off Vocal).mp3"
+    ],
+    "放課後クライマックスガールズ": [
+      "M-Scarlet - キラピコ.exe (Off Vocal).mp3",
+      "M-Scarlet - ハナサカサイサイ (Off Vocal).mp3",
+      "M-Scarlet - 快盗Vを見逃すな (Off Vocal).mp3",
+      "M-Scarlet - 疾風ザブングル (Off Vocal).mp3"
+    ]
+  }
+};
